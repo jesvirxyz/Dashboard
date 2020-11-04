c01ed775418bd8ad5321618eb8ec1566c9e49e74
Jesvir Subang <jesvirxyz@gmail.com>
2020-11-04 17:59:22 +0800

    complete sales

--- business/dashboard-sales.business.js
+++ business/dashboard-sales.business.js
@@ -17,7 +17,7 @@ exports.getSalesData = async (data) => {
 
 	let location = ['5f5f3e8782303800122d02f7', '5f6ee433e365e80019f5fe8a'];
 	// let location = ['5f5f3e8782303800122d02f7'];
-	let start_date = '2020-10-29T05:30:00.000Z';
+	let start_date = '2020-09-29T05:30:00.000Z';
 	let end_date = '2020-10-30T05:30:00.000Z';
 	// let location = body.locationId;
 	// let start_date = body.start_date;
@@ -25,22 +25,56 @@ exports.getSalesData = async (data) => {
 	let ownerId = mongoose.Types.ObjectId(data.owner);
 
 	let startOfYear = moment(start_date).startOf('year');
-	let endOfYear = moment(end_date).endOf('year');
+	let endOfYear = moment(start_date).endOf('year');
 	let startOfWeek = moment(start_date).startOf('week');
-	let endOfWeek = moment(end_date).endOf('week');
+	let endOfWeek = moment(start_date).endOf('week');
 	let startOfMonth = moment(start_date).startOf('month');
-	let endOfMonth = moment(end_date).endOf('month');
+	let endOfMonth = moment(start_date).endOf('month');
+
+	let compareStartOfYear = moment(end_date).startOf('year');
+	let compareEndOfYear = moment(end_date).endOf('year');
+	let compareStartOfWeek = moment(end_date).startOf('week');
+	let compareEndOfWeek = moment(end_date).endOf('week');
+	let compareStartOfMonth = moment(end_date).startOf('month');
+	let compareEndOfMonth = moment(end_date).endOf('month');
 
 	try {
 		let salesQuery = [];
 		let salesTotal = 0;
+		let compareSalesTotal = 0;
 		let graphData = [];
+		let compareGraphData = [];
 		let result = [];
 		let finalResult;
 		if (location.length > 0) {
 			for (let i = 0; i < location.length; i++) {
 				//Monthly view
-				if (data.query.monthly) {
+				if (data.query.month) {
+					appointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(startOfMonth),
+									$lte: new Date(endOfMonth),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
 					salesQuery = [
 						{
 							$match: {
@@ -67,20 +101,112 @@ exports.getSalesData = async (data) => {
 						{ $sort: { _id: 1 } },
 					];
 
+					//Comapre Query
+					compareAppointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(compareStartOfMonth),
+									$lte: new Date(compareEndOfMonth),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+					compareSalesQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								location: mongoose.Types.ObjectId(location[i]),
+								updatedAt: {
+									$gte: new Date(compareStartOfMonth),
+									$lte: new Date(compareEndOfMonth),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$updatedAt',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$totalAmount' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+
+					let compareSalesResult = await Sales.aggregate(
+						compareSalesQuery
+					);
+
+					compareSalesResult.forEach((element) => {
+						let data = {
+							valueKey: element._id,
+							amount: element.totalAmount,
+						};
+
+						compareSalesTotal =
+							compareSalesTotal + element.totalAmount;
+						compareGraphData.push(data);
+					});
 					let salesResult = await Sales.aggregate(salesQuery);
 					salesResult.forEach((element) => {
 						let data = {
 							valueKey: element._id,
 							amount: element.totalAmount,
 						};
+
 						salesTotal = salesTotal + element.totalAmount;
 						graphData.push(data);
 					});
 				}
 				//Yearly view
-				if (data.query.yearly) {
+				if (data.query.year) {
 					console.log('Yearly is selected');
 
+					appointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(startOfYear),
+									$lte: new Date(endOfYear),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
 					salesQuery = [
 						{
 							$match: {
@@ -106,10 +232,67 @@ exports.getSalesData = async (data) => {
 						},
 						{ $sort: { _id: 1 } },
 					];
+					//Comapre Query
+					compareAppointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(compareStartOfYear),
+									$lte: new Date(compareEndOfYear),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+					compareSalesQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								location: mongoose.Types.ObjectId(location[i]),
+								updatedAt: {
+									$gte: new Date(compareStartOfYear),
+									$lte: new Date(compareEndOfYear),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$updatedAt',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$totalAmount' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+
+					let compareSalesResult = await Sales.aggregate(
+						compareSalesQuery
+					);
 
 					let salesResult = await Sales.aggregate(salesQuery);
 					let count = 0;
 					let prev;
+					let compare_count = 0;
+					let compare_prev;
 					const months = [
 						'January',
 						'February',
@@ -159,9 +342,79 @@ exports.getSalesData = async (data) => {
 							graphData.push(data);
 						}
 					});
+
+					compareSalesResult.forEach((element) => {
+						let month = new Date(element._id).getMonth();
+						if (
+							compare_prev == month ||
+							typeof compare_prev == 'undefined'
+						) {
+							for (let i = 0; i < 12; i++) {
+								if (
+									typeof compare_prev == undefined ||
+									month == i
+								) {
+									compareSalesTotal =
+										compareSalesTotal + element.totalAmount;
+									compare_prev = month;
+								}
+							}
+							compare_count++;
+						} else if (compare_prev < month) {
+							let data = {
+								valueKey: months[month - 1],
+								amount: compareSalesTotal,
+							};
+							compareGraphData.push(data);
+							compareSalesTotal = 0;
+							for (let i = 0; i < 12; i++) {
+								if (
+									typeof compare_prev == 'undefined' ||
+									month == i
+								) {
+									compareSalesTotal =
+										compareSalesTotal + element.totalAmount;
+									compare_prev = month;
+								}
+							}
+							compare_count++;
+						}
+						if (compare_count == compareSalesResult.length) {
+							let data = {
+								valueKey: months[month],
+								amount: compareSalesTotal,
+							};
+							compareGraphData.push(data);
+						}
+					});
 				}
 				//Weekly view
-				if (data.query.weekly) {
+				if (data.query.week) {
+					appointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(startOfWeek),
+									$lte: new Date(endOfWeek),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
 					salesQuery = [
 						{
 							$match: {
@@ -188,6 +441,73 @@ exports.getSalesData = async (data) => {
 						{ $sort: { _id: 1 } },
 					];
 
+					//Comapre Query
+					compareAppointmentQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								status: 'completed',
+								day: {
+									$gte: new Date(compareStartOfWeek),
+									$lte: new Date(compareEndOfWeek),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$day',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$price' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+					compareSalesQuery = [
+						{
+							$match: {
+								ownerId: mongoose.Types.ObjectId(ownerId),
+								location: mongoose.Types.ObjectId(location[i]),
+								updatedAt: {
+									$gte: new Date(compareStartOfWeek),
+									$lte: new Date(compareEndOfWeek),
+								},
+							},
+						},
+						{
+							$group: {
+								_id: {
+									$dateToString: {
+										format: '%Y-%m-%d',
+										date: '$updatedAt',
+									},
+								},
+								count: { $sum: 1 },
+								totalAmount: { $sum: '$totalAmount' },
+							},
+						},
+						{ $sort: { _id: 1 } },
+					];
+
+					let compareSalesResult = await Sales.aggregate(
+						compareSalesQuery
+					);
+
+					compareSalesResult.forEach((element) => {
+						let data = {
+							valueKey: element._id,
+							amount: element.totalAmount,
+						};
+
+						compareSalesTotal =
+							compareSalesTotal + element.totalAmount;
+						compareGraphData.push(data);
+					});
+
 					let salesResult = await Sales.aggregate(salesQuery);
 					salesResult.forEach((element) => {
 						let data = {
@@ -198,12 +518,40 @@ exports.getSalesData = async (data) => {
 						graphData.push(data);
 					});
 				}
+				compareAppointmentQuery[0].$match.location = mongoose.Types.ObjectId(
+					location[i]
+				);
+				appointmentQuery[0].$match.location = mongoose.Types.ObjectId(
+					location[i]
+				);
+				let appointmentResult = await Appointment.aggregate(
+					appointmentQuery
+				);
+				let compareAppointmentResult = await Appointment.aggregate(
+					compareAppointmentQuery
+				);
+				let appointmentCount = 0;
+				let compareAppointmentCount = 0;
+				appointmentResult.forEach((element) => {
+					appointmentCount = appointmentCount + element.count;
+				});
+				compareAppointmentResult.forEach((element) => {
+					compareAppointmentCount =
+						compareAppointmentCount + element.count;
+				});
+
 				finalResult = {
 					location: location[i],
+					appointmentCount: appointmentCount,
+					compareAppointmentCount: compareAppointmentCount,
 					sales: graphData,
+					compareSales: compareGraphData,
 				};
 				result.push(finalResult);
 				graphData = [];
+				compareGraphData = [];
+				appointmentCount = 0;
+				compareAppointmentCount = 0;
 			}
 			return result;
 		} else {
@@ -213,3 +561,4 @@ exports.getSalesData = async (data) => {
 		throw error;
 	}
 };
+
